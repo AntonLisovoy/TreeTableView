32ee4223b99f1eb3ba6c6dd621f5d2cc623960ea
Антон Лисовой <lisovoy@arcsinus.ru>
2019-07-17 14:07:56 +0300

    Initial Commit

---
+++ TreeTableView/TreeTableView.h
@@ -0,0 +1,19 @@
+//
+//  TreeTableView.h
+//  TreeTableView
+//
+//  Created by Anton Lisovoy on 17/07/2019.
+//  Copyright © 2019 Anton Lisovoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TreeTableView.
+FOUNDATION_EXPORT double TreeTableViewVersionNumber;
+
+//! Project version string for TreeTableView.
+FOUNDATION_EXPORT const unsigned char TreeTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TreeTableView/PublicHeader.h>
+
+
